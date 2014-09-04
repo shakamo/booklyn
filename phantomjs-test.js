f423c0802abd863b3e5024eb28a2e0a5ef8b2d7b
shakamo <sakamoto.jun05@gmail.com>
2014-09-04 02:08:51 -0400

    Add today's work.

--- phantomjs-test.js
+++ phantomjs-test.js
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-
-<html lang="en">
-  <head>
-
-  </head>
-
-  <body>
-    <script type="text/javascript">
-      console.log('Loading a web page');
-      var page = require('webpage').create();
-      var url = 'http://www.phantomjs.org/';
-      page.open(url, function(status) {
-        //Page is loaded!
-        phantom.exit();
-      });
-    </script>
-
-  </body>
-</html>
+console.log('Loading a web page');
+var page = require('webpage').create();
+var url = 'http://www.phantomjs.org/';
+page.open(url, function(status) {
+  // Page is loaded!
+  phantom.exit();
+});
